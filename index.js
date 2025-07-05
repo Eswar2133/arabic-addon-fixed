4faea4ae2478179d6d336901b24ba47ef7fa46d7
Eswar2133 <kuwait2412@gmail.com>
2025-07-05 18:17:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import sdk from 'stremio-addon-sdk';
 const { addonBuilder } = sdk;
 
-// (rest of your addon code continues)
-import { addonBuilder } from "stremio-addon-sdk";
-
 const manifest = {
   id: "org.arabic.addon",
   version: "1.0.0",
@@ -20,56 +17,7 @@ const manifest = {
   resources: ["catalog", "stream"],
 };
 
-const builder = new addonBuilder(manifest);
-
-// Sample catalog handler
-builder.defineCatalogHandler(({ type, id, extra }) => {
-  return Promise.resolve({
-    metas: [
-      {
-        id: "movie1",
-        type: "movie",
-        name: "Arabic Movie 1",
-        poster: "https://via.placeholder.com/300x450.png?text=Arabic+Movie+1",
-      },
-    ],
-  });
-});
-
-// Sample stream handler
-builder.defineStreamHandler(({ type, id }) => {
-  if (id === "movie1") {
-    return Promise.resolve({
-      streams: [
-        {
-          title: "Arabic Movie Stream",
-          url: "https://example.com/arabic-movie1.mp4",
-        },
-      ],
-    });
-  }import sdk from 'stremio-addon-sdk';
-const { addonBuilder } = sdk;
-
-// (rest of your addon code continues)
-import { addonBuilder } from "stremio-addon-sdk";
-
-const manifest = {
-  id: "org.arabic.addon",
-  version: "1.0.0",
-  name: "Arabic Addon",
-  description: "Arabic movies and shows for Stremio",
-  types: ["movie"],
-  catalogs: [
-    {
-      type: "movie",
-      id: "arabic",
-      name: "Arabic Movies",
-    },
-  ],
-  resources: ["catalog", "stream"],
-};
-
-const builder = new addonBuilder(manifest);
+const builder = addonBuilder(manifest);
 
 // Sample catalog handler
 builder.defineCatalogHandler(({ type, id, extra }) => {
@@ -97,12 +45,6 @@ builder.defineStreamHandler(({ type, id }) => {
       ],
     });
   }
-
-  return Promise.resolve({ streams: [] });
-});
-
-export default builder.getInterface();
-
 
   return Promise.resolve({ streams: [] });
 });
