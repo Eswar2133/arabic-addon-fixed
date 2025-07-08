60ffb69022d86f010ae3fef1eb6c1c2851a4bdeb
Eswar2133 <kuwait2412@gmail.com>
2025-07-08 21:09:58 +0300

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -3,8 +3,8 @@ const express = require("express");  // Import Express
 
 const app = express();  // Create an Express app
 
-// Correctly instantiate the addon using new sdk.Addon()
-const addon = new sdk.Addon({
+// Correctly use the addonBuilder function to create the addon
+const addon = sdk.addonBuilder({
   id: "org.arabic.addon",
   version: "1.0.0",
   name: "Arabic Addon",
